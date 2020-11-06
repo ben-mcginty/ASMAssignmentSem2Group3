11d7068c991a0f8c39b45896eacc4a97ad34aef5
Ben McGinty <ben.a.mcginty@gmail.com>
2020-11-06 14:19:46 +1100

    Update sending.py

--- sending.py
+++ sending.py
@@ -3,20 +3,22 @@
 
 from microbit import *
 import radio
-import random
 
-channel = random.randrange(1,100,0)
 radio.on()
-radio.config(channel=channel)
+channelNum = 0                          
+radio.config(channel=channelNum)
 radio.config(power=7)
+channelConnected = False
 
-display.scroll(channel)
-
-# Event loop.
-for i in channel:
-    radio.send("Attempting link")
-    dataRecieved = radio.receive()
-    if dataRecieved == "Connected":
-        radio.send("Correct channel")
+while channelConnected == False:
+    message = radio.receive()
+    print(str(channelNum))
+    if message == "Attempting link":
+        channelConnected = True
+        radio.send("Connected")
         display.show(Image.YES)
-        break
+    elif channelNum != 83:
+        channelNum += 1
+        radio.config(channel=channelNum)
+    else:
+        channelNum = 0
