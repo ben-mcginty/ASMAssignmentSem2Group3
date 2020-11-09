db8778ae45612db5a5f33624c3df058570c5b7fd
Placid-PoTaTo <73676092+Placid-PoTaTo@users.noreply.github.com>
2020-11-09 14:43:20 +1100

    Update and rename reciving.py to receiving.py

--- receiving.py
+++ receiving.py
@@ -1,22 +1,26 @@
 # This software is distributed to you under the licence set out in the GNU General Public Licence 3.0.
 # Licence avaliable at https://www.gnu.org/licenses/gpl-3.0
 
-from microbit import *
+#Import libraries
+from microbit import * 
 import radio
 
+#setting up radio
 radio.on()
 channelNum = 0                          
 radio.config(channel=channelNum)
 radio.config(power=7)
 channelConnected = False
 
+#event loop
 while channelConnected == False:
-    message = radio.receive()
-    print(str(channelNum))
+    message = radio.receive() #recieving message
+    print(str(channelNum)) #printing the channel number to console
     if message == "Attempting link":
-        channelConnected = True
-        radio.send("Connected")
+        channelConnected = True #see if channel is received
+        radio.send("Connected") 
         display.show(Image.YES)
+        #increment channel by 1 if below 83 otherwise 0
     elif channelNum != 83:
         channelNum += 1
         radio.config(channel=channelNum)
